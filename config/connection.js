52c99e279cc5f99b51397e32a12267f0674436d0
jayarghargh <jayarghargh@gmail.com>
2021-02-20 11:39:29 +1000

    added models and tweaked queries in template

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
 	user: 'root',
 	// NOTE: Be sure to add your MySQL password here!
 	password: 'himypal',
-	database: 'burgers',
+	database: 'burgers_db',
 });
 
 // Make connection.
