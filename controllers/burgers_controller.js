2974ec3ed966ec72ad7b60306e14bad537138342
jayarghargh <jayarghargh@gmail.com>
2021-02-21 12:07:53 +1000

    added bootstrap for styling

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -24,7 +24,7 @@ router.get('/api/', (req, res) => {
 
 
 router.post('/api/burgers', (req, res) => {
-	burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], (result) => {
+	burger.create(['burger_name'], [req.body.burger_name], (result) => {
 		// Send back the ID of the new quote
 		res.json({ id: result.insertId });
 	});
